5e7821c76c80d7da488b96d007ed9ae23a0d4194
mohamed charfi <100835078+charfimohamed@users.noreply.github.com>
2024-05-24 16:04:43 +0200

    last version

--- docs/js/playerSearchScript.js
+++ docs/js/playerSearchScript.js
@@ -29,12 +29,17 @@ document.getElementById('searchBar').addEventListener('keydown', function(event)
     }
 });
 
+
+
+
 // Highlight the selected suggestion
 function highlightSelection(suggestions, index) {
     suggestions.forEach((div, idx) => {
         div.style.backgroundColor = idx === index ? '#f0f0f0' : ''; // Highlight only the selected suggestion
     });
 }
+
+
 
 // Display player data when a player name is selected
 function displayPlayer(playerName) {
@@ -55,7 +60,6 @@ function displayPlayer(playerName) {
                 document.getElementById('defending').textContent = `DEF: ${parseInt(player['defensive_metric'])}`;
                 document.getElementById('position').textContent = player['Best Position_fifa'];
 
-                // Create a detailed and formatted description of the player
                 const playerInfo = `
                     Age: ${player['MP']} years<br>
                     Height: ${player['Height(in cm)_fifa']} cm<br>
@@ -118,6 +122,116 @@ document.getElementById('searchBar').addEventListener('input', function() {
         suggestionsPanel.style.display = 'none';
     }
 });
+
+
+
+let selectedIndex2 = -1; // Index of the currently highlighted player in the suggestions list
+
+document.getElementById('searchBar2').addEventListener('keydown', function(event) {
+    const suggestionsPanel = document.getElementById('suggestions2');
+    const suggestions = suggestionsPanel.querySelectorAll('div');
+
+    if (event.key === 'ArrowDown') {
+        if (selectedIndex2 < suggestions.length - 1) {
+            selectedIndex2++;
+            highlightSelection(suggestions, selectedIndex2);
+        }
+        event.preventDefault();
+    } else if (event.key === 'ArrowUp') {
+        if (selectedIndex2 > 0) {
+            selectedIndex2--;
+            highlightSelection(suggestions, selectedIndex2);
+        }
+        event.preventDefault();
+    } else if (event.key === 'Enter') {
+        if (selectedIndex2 > -1 && suggestions[selectedIndex2]) {
+            suggestions[selectedIndex2].click();
+        }
+        event.preventDefault();
+    }
+});
+
+
+document.getElementById('searchBar2').addEventListener('input', function() {
+    console.log("listeeerrrrrrrrrr");
+    let input = this.value.toLowerCase();
+    let suggestionsPanel = document.getElementById('suggestions2');
+    if (input.length > 2) {
+        fetch('data/finalPlayerDf.json')
+            .then(response => response.json())
+            .then(players => {
+                const filteredPlayers = players.filter(p => p.Player.toLowerCase().includes(input));
+                suggestionsPanel.innerHTML = '';
+                filteredPlayers.forEach(player => {
+                    const div = document.createElement('div');
+                    div.textContent = player.Player;
+                    div.addEventListener('click', function() {
+                        displayPlayer2(player.Player);
+                        document.getElementById('searchBar2').value = '';
+                        suggestionsPanel.style.display = 'none';
+                        selectedIndex2 = -1;
+                    });
+                    div.addEventListener('mouseover', function() {
+                        selectedIndex2 = Array.from(suggestionsPanel.children).indexOf(div);
+                        highlightSelection(suggestionsPanel.querySelectorAll('div'), selectedIndex2);
+                    });
+                    div.addEventListener('mouseout', function() {
+                        div.style.backgroundColor = '';
+                    });
+                    suggestionsPanel.appendChild(div);
+                });
+                if (filteredPlayers.length > 0) {
+                    suggestionsPanel.style.display = 'block';
+                } else {
+                    suggestionsPanel.style.display = 'none';
+                }
+            }).catch(error => {
+            console.error('Error fetching or processing data:', error);
+            suggestionsPanel.innerHTML = '';
+            suggestionsPanel.style.display = 'none';
+        });
+    } else {
+        suggestionsPanel.innerHTML = '';
+        suggestionsPanel.style.display = 'none';
+    }
+});
+
+
+function displayPlayer2(playerName) {
+    fetch('data/finalPlayerDf.json')
+        .then(response => response.json())
+        .then(players => {
+            let player = players.find(p => p.Player === playerName);
+            if (player) {
+                document.getElementById('playerCard2').style.display = 'block';
+                document.getElementById('playerName2').textContent = playerName;
+                document.getElementById('playerImage2').src = `data/players_images/${playerName}.png`;
+                document.getElementById('nationImage2').src = `data/flags/${player['Nation']}.png`;
+                document.getElementById('pace2').textContent = `PAC: ${parseInt(player['pace_metric'])}`;
+                document.getElementById('shooting2').textContent = `SHO: ${parseInt(player['shooting_metric'])}`;
+                document.getElementById('dribbling2').textContent = `DRI: ${parseInt(player['dribble_metric'])}`;
+                document.getElementById('physique2').textContent = `PHY: ${parseInt(player['physical_metric'])}`;
+                document.getElementById('passing2').textContent = `PAS: ${parseInt(player['passing_metric'])}`;
+                document.getElementById('defending2').textContent = `DEF: ${parseInt(player['defensive_metric'])}`;
+                document.getElementById('position2').textContent = player['Best Position_fifa'];
+
+                const playerInfo = `
+                    Age: ${player['MP']} years<br>
+                    Height: ${player['Height(in cm)_fifa']} cm<br>
+                    Position: ${player['Best Position_fifa']}<br>
+                    Market Value: ${player['Value(in Euro)_fifa']} euros<br>
+                    Wage: ${player['Wage(in Euro)_fifa']} euros per week
+                `;
+                document.getElementById('playerInfo2').innerHTML = playerInfo; // Use innerHTML to render the line breaks
+                document.getElementById('playerDetails2').style.display = 'block';
+            } else {
+                document.getElementById('playerName2').textContent = "Player not found";
+                document.getElementById('playerCard2').style.display = 'none';
+                document.getElementById('playerDetails2').style.display = 'none';
+            }
+        });
+}
+
 
 
 
